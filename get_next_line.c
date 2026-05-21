66ab557092dbec2f10460343624769c13669d81e
Andre Miguel Chen <andmigue@c2r5s12.42lisboa.com>
2026-05-21 17:32:00 +0100

    check

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: andmigue <andmigue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 18:47:49 by andmigue          #+#    #+#             */
-/*   Updated: 2026/05/20 19:36:30 by andmigue         ###   ########.fr       */
+/*   Updated: 2026/05/21 17:24:24 by andmigue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 char *get_next_line(int fd)
 {
-	
+	metodo infalivel - static char buffer[BUFFER_SIZE + 1]
+	open read close 
 }
